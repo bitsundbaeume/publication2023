ea9aec8d1b302eca4c578c659a7d224332e0964d
Felix Scholze <felix@webshaped.de>
2023-05-17 00:25:32 +0200

    build: basic astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,32 @@
 import { defineConfig } from 'astro/config';
+import path, { resolve, dirname } from 'node:path';
+import { fileURLToPath } from 'url';
+import sitemap from '@astrojs/sitemap';
+import prefetch from '@astrojs/prefetch';
+import critters from "astro-critters";
+import serviceWorker from "astrojs-service-worker";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://publication2023.bits-und-baeume.org',
+  integrations: [
+    sitemap({
+      lastmod: new Date(),
+    }),
+    prefetch(),
+    critters(),
+    serviceWorker()
+  ],
+  vite: {
+    plugins: [],
+    resolve: {
+      alias: {
+        '@sass-butler/': `${path.resolve(__dirname, 'node_modules/@felix_berlin/sass-butler/')}/`,
+        '@styles/': `${path.resolve(__dirname, 'src/styles/')}/`
+      }
+    }
+  },
+});
