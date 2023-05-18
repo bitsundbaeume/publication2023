229d9886c4715cd793d127eff9b75326e9fc04c7
Felix Scholze <felix@webshaped.de>
2023-05-18 13:57:39 +0200

    feat: finalize footer layout

---
+++ src/components/footer/logos.ts
@@ -0,0 +1,133 @@
+import type { Logo } from "@components/footer/types";
+
+export const supporters: Logo[] = [
+  {
+    name: "Deutscher Naturschutzring",
+    logo: "src/images/logos/traegerinnen/dnr.svg",
+    url: "https://www.dnr.de/",
+    alt: "Deutscher Naturschutzring Logo",
+  },
+  {
+    name: "Institut für ökologische Wirtschaftsforschung",
+    logo: "src/images/logos/traegerinnen/ioew.svg",
+    url: "https://www.ioew.de/",
+    alt: "Institut für ökologische Wirtschaftsforschung Logo",
+  },
+  {
+    name: "Technische Universität Berlin",
+    logo: "src/images/logos/traegerinnen/tu-berlin.svg",
+    url: "https://www.tu-berlin.de/",
+    alt: "Technische Universität Berlin Logo",
+  },
+  {
+    name: "Germanwatch e.V.",
+    logo: "src/images/logos/traegerinnen/germanwatch.svg",
+    url: "https://germanwatch.org/",
+    alt: "Germanwatch e.V. Logo",
+  },
+  {
+    name: "BUND",
+    logo: "src/images/logos/traegerinnen/bund.svg",
+    url: "https://www.bund.net/",
+    alt: "BUND Logo",
+  },
+  {
+    name: "Brot für die Welt",
+    logo: "src/images/logos/traegerinnen/brot-fuer-die-welt.svg",
+    url: "https://www.brot-fuer-die-welt.de/",
+    alt: "Brot für die Welt Logo",
+  },
+  {
+    name: "Chaos Computer Club",
+    logo: "src/images/logos/traegerinnen/ccc.svg",
+    url: "https://www.ccc.de/",
+    alt: "Chaos Computer Club Logo",
+  },
+  {
+    name: "Forum InformatikerInnen für Frieden und gesellschaftliche Verantwortung",
+    logo: "src/images/logos/traegerinnen/fiff-schwarz.svg",
+    url: "https://www.fiff.de/",
+    alt: "Forum InformatikerInnen für Frieden und gesellschaftliche Verantwortung Logo",
+  },
+  {
+    name: "konzeptwerk neue ökonomie",
+    logo: "src/images/logos/traegerinnen/konzeptwerk.svg",
+    url: "https://www.konzeptwerk-neue-oekonomie.org/",
+    alt: "konzeptwerk neue ökonomie Logo",
+  },
+  {
+    name: "Open Knowledge Foundation",
+    logo: "src/images/logos/traegerinnen/okfn.svg",
+    url: "https://okfn.de/",
+    alt: "Open Knowledge Foundation Logo",
+  },
+  {
+    name: "Einstein Center Digital Future",
+    logo: "src/images/logos/traegerinnen/einstein_center.svg",
+    url: "https://www.digital-future.berlin/",
+    alt: "Einstein Center Digital Future Logo",
+  },
+  {
+    name: "Free Software Foundation Europe",
+    logo: "src/images/logos/traegerinnen/fsfe.svg",
+    url: "https://fsfe.org/",
+    alt: "Free Software Foundation Europe Logo",
+  },
+  {
+    name: "Weizenbaum-Institut",
+    logo: "src/images/logos/traegerinnen/weizenbaum.png",
+    url: "https://weizenbaum-institut.de/",
+    alt: "Weizenbaum-Institut Logo",
+  },
+];
+
+export const media: Logo[] = [
+  {
+    name: "Reset",
+    logo: "src/images/logos/foerderinnen/logo-reset.svg",
+    url: "https://reset.org/",
+    alt: "Reset Logo",
+  },
+  {
+    name: "Heise",
+    logo: "src/images/logos/foerderinnen/logo-heise-online.svg",
+    url: "https://www.heise.de/",
+    alt: "Heise Logo",
+  },
+];
+
+export const specialThanks: Logo[] = [
+  {
+    name: "Cleaner Web",
+    logo: "src/images/logos/thanks_to/logo-cleaner-web.svg",
+    url: "https://cleaner-web.com/",
+    alt: "Cleaner Web Logo",
+  },
+  {
+    name: "Uber Space",
+    logo: "src/images/logos/thanks_to/logo-uberspace.svg",
+    url: "https://uberspace.de/",
+    alt: "Uber Space Logo",
+  },
+];
+
+export const funded: Logo[] = [
+  {
+    name: "DBU",
+    logo: "src/images/logos/foerderinnen/logo-dbu.png",
+    url: "https://www.dbu.de/",
+    alt: "DBU Logo",
+  },
+  {
+    name: "Stiftung Mercator",
+    logo: "src/images/logos/foerderinnen/Stiftung_Mercator_Blau_RGB.png",
+    url: "https://www.stiftung-mercator.de/",
+    alt: "Stiftung Mercator Logo",
+  },
+  {
+    name: "Bundesministerium für Umwelt, Naturschutz und nukleare Sicherheit",
+    logo: "src/images/logos/foerderinnen/BMUV.png",
+    url: "https://www.bmu.de/",
+    alt: "Bundesministerium für Umwelt, Naturschutz und nukleare Sicherheit Logo",
+  },
+];
