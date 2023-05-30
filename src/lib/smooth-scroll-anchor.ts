4c642b20cec14181920a76ce620f0754c4fe51e2
Felix Scholze <felix@webshaped.de>
2023-05-31 01:03:50 +0200

    refactor: smooth scroll

--- src/lib/smooth-scroll-anchor.ts
+++ src/lib/smooth-scroll-anchor.ts
@@ -26,33 +26,20 @@ const smoothScrollTo = (target: HTMLElement | string, event?: Event): void => {
  *
  * @param   {string}  selector  DOM selector
  *
- */
-const queryCheck = (selector: string) => document.createDocumentFragment().querySelector(selector)
-
-/**
- * Check if a selector is valid
- *
- * @param   {string}  selector  DOM selector
- *
  * @return  {bool}
  */
 const isSelectorValid = (selector: string): Boolean => {
-  try { queryCheck(selector) }
-  catch { return false }
-  return true
+  return document.querySelector(selector) !== null
 }
 
 export default (() => {
-  window.addEventListener('hashchange', () => {
+  window.addEventListener('popstate', () => {
     if (window.location.hash.match(/#(\d+)/)) {
       smoothScrollTo('#flipbook')
     }
   })
 
   window.addEventListener('click', event => {
-    const target = event.target as HTMLElement;
-
-    if (target?.tagName?.toLowerCase() !== 'a') return;
     smoothScrollTo(event.target as HTMLElement, event);
   })
 })();
