4006cad362925794e461a45d7a625ff766b63ca7
Felix Scholze <felix@webshaped.de>
2023-06-04 13:29:41 +0200

    feat: add book toc content

---
+++ src/components/book-toc-types.ts
@@ -0,0 +1,14 @@
+export interface BookToc {
+  toc: [
+    {
+      headline?: string;
+      contents?: [
+        {
+          title: string;
+          url: string;
+          pageNumber?: number;
+        }
+      ]
+    }
+  ];
+}
---
+++ src/lib/content/book-toc.ts
@@ -0,0 +1,197 @@
+// import type Props from "@components/BookToc.astro";
+
+export const flipBookToc: object[] = [
+  {
+    headline: "0 Introduction",
+    contents: [
+      {
+        title: "Editorial - P. Jankowski, A. Höfner & M. L. Hoffmann",
+        url: "#1",
+        pageNumber: null,
+      },
+      {
+        title: "Preface - T. Santarius & N. Guenot",
+        url: "#1",
+        pageNumber: null,
+      }
+    ]
+  },
+  {
+    headline: "1 Redefining Progress: Confronting the Challenges of Digitalisation and Sustainability",
+    contents: [
+      {
+        title: "Challenges of Digitalisation and Sustainability at a Glance - Infographic - F. Rohde & P. Jankowski",
+        url: "#1",
+        pageNumber: null,
+      },
+      {
+        title: "What Your Data Discloses About Others - L. Fröberg",
+        url: "#1",
+        pageNumber: null,
+      },
+      {
+        title: "No Blockchains On A Dead Planet - P. Robben & F. Hildebrandt",
+        url: "#1",
+        pageNumber: null,
+      },
+      {
+        title: "Artificial Intelligence for Real Sustainability - R. Rehak",
+        url: "#1",
+        pageNumber: null,
+      },
+      {
+        title: "The Invisible Environmental Impact of Mobile Apps - F. Petri & M. Ruhenstroth",
+        url: "#1",
+        pageNumber: null,
+      },
+      {
+        title: "Transparency for Software Climate Impact - S. Kruijer, A. Tarara & M. Schulze",
+        url: "#1",
+        pageNumber: null,
+      },
+    ]
+  },
+  {
+    headline: "2 Values for Transformation: Foundation for a Socio-Ecological Digitalisation",
+    contents: [
+      {
+        title: "A Feminist Reminder in Times of Digitalisation - H. Völkle & E. Lindinger",
+        url: "#1",
+        pageNumber: null,
+      },
+      {
+        title: "How to Grow Alternative Platforms (Fast) - G. Franke & J. Pentzien",
+        url: "#1",
+        pageNumber: null,
+      },
+      {
+        title: "An Office Without GAFAM? - V. Frick et al.",
+        url: "#1",
+        pageNumber: null,
+      },
+      {
+        title: "Infrastructures as Commons - J. U. Hasecke & M. Hierweck",
+        url: "#1",
+        pageNumber: null,
+      },
+      {
+        title: "The Digital Powerwash - B. Wijers",
+        url: "#1",
+        pageNumber: null,
+      },
+    ]
+  },
+  {
+    headline: "3 Transformative Change: New Concepts and Paradigms to Update and Replace Current Socio-Technical Structures",
+    contents: [
+      {
+        title: "Open Source Hardware and Open Design - L. Zimmermann & M. Voigt",
+        url: "#1",
+        pageNumber: null,
+      },
+      {
+        title: "On Digital Sustainability and Digital Public Goods - M. Stürmer, M. Tiede, J. Nussbaumer & F. Wäspi",
+        url: "#1",
+        pageNumber: null,
+      },
+      {
+        title: "From Digital Cannibalism to a New Food Pyramid - N. Rahman",
+        url: "#1",
+        pageNumber: null,
+      },
+      {
+        title: "Business of Deep Transformations - I. Nesterova, L. Beyeler & L. Niessen",
+        url: "#1",
+        pageNumber: null,
+      },
+      {
+        title: "Towards Artful Sustainable Integration of IT Infrastructures - E. Sørensen & S. Laser",
+        url: "#1",
+        pageNumber: null,
+      },
+    ]
+  },
+  {
+    headline: "4 Beyond Business as Usual: Use Cases for More Sustainable Tech Design",
+    contents: [
+      {
+        title: "Computer Scientists for Future - E. Eickstädt, M. Becke, M. Kohler & J. Padberg",
+        url: "#1",
+        pageNumber: null,
+      },
+      {
+        title: "How Digitalisation Can Help Neighbourhoods Share Electricity - A. Aretz & I. Jungblut",
+        url: "#1",
+        pageNumber: null,
+      },
+      {
+        title: "Digitalisation for a Socio-Ecological Transformation in Agriculture - L. Prause & A. Egger",
+        url: "#1",
+        pageNumber: null,
+      },
+      {
+        title: "Workshop Results - melzai",
+        url: "#1",
+        pageNumber: null,
+      },
+    ]
+  },
+  {
+    headline: "5 Politics Under Pressure: Necessary Changes on the Political Level",
+    contents: [
+      {
+        title: "Bits & Bäume Political Demands at a Glance - Infographic - M. L. Hoffmann & J. Graf",
+        url: "#1",
+        pageNumber: null,
+      },
+      {
+        title: "Making a More Sustainable Telecom Sector with Free Software - L. Lasota & E. Albers",
+        url: "#1",
+        pageNumber: null,
+      },
+      {
+        title: "It's Time for Bold Policy-Making - H. Zimmermann & J. Pohl",
+        url: "#1",
+        pageNumber: null,
+      },
+      {
+        title: "New Government for New Tasks? - S. Ramesohl, J. Wirtz, A. Gunnemann & R. Weier",
+        url: "#1",
+        pageNumber: null,
+      },
+    ]
+  },
+  {
+    headline: "6 A Collective Effort: The Power of Civil Society Driving Sustainable Change",
+    contents: [
+      {
+        title: "Why Designing a Sustainable Digital Future Requires Policy-Makers to Include Civil Society - V. Frick, A. Mollen & F. Rohde",
+        url: "#1",
+        pageNumber: null,
+      },
+      {
+        title: "Preventing Surveillance Capitalism from Hijacking Socio-Ecological Transformations - L. Hennecke & K. Jung",
+        url: "#1",
+        pageNumber: null,
+      },
+      {
+        title: "Crowdacting as a Spark for Climate Protection? - B. Parske & K. Kastner",
+        url: "#1",
+        pageNumber: null,
+      },
+      {
+        title: "Hypernuding? - E. Lamura & M. Lamura",
+        url: "#1",
+        pageNumber: null,
+      },
+      {
+        title: "How Can I Get Involved? Take Action! - N. Duddek & A. Höfner",
+        url: "#1",
+        pageNumber: null,
+      },
+    ]
+  },
+  {
+    headline: "Acknowledgements",
+  },
+];
