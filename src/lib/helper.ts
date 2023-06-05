066a3b828a5b4404441a5e1b28006868b85019e8
Felix Scholze <felix.scholze@hyperlink.eu>
2023-06-05 19:14:12 +0200

    fix: filter non images formats from flipbook src.

---
+++ src/lib/helper.ts
@@ -0,0 +1,18 @@
+/**
+ * Return file extension from string
+ *
+ * @param   {string}  filename  [filename description]
+ *
+ * @return  {string}            [return description]
+ */
+export const extensionIs = (filename: string): string => {
+  const regex = /(?:\.([^.]+))?$/;
+
+  return regex.exec(filename)![1];
+};
+
+export const replaceExtension = (filename: string, newExtension: string): string => {
+  const regex = /(?:\.([^.]+))?$/;
+
+  return filename.replace(regex, newExtension);
+};
