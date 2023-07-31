5ca38810ecb8a55ca946955bad3d7fd2e67666ee
Felix Scholze <felix@webshaped.de>
2023-07-31 22:09:18 +0200

    feat: update content config + add headerImage

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,12 +3,19 @@ import { z, defineCollection } from "astro:content";
 const publication2023Collection = defineCollection({
   type: "content",
   schema: z.object({
-    isDraft: z.boolean(),
+    isDraft: z.boolean().optional(),
     title: z.string().min(1),
     authors: z.array(z.string()).optional(),
     tag: z.string().optional(),
     chapter: z.number(),
     order: z.number(),
+    headerImage: z
+      .object({
+        src: z.string(),
+        alt: z.string().optional(),
+        caption: z.string().optional(),
+      })
+      .optional(),
     seo: z
       .object({
         title: z.string().optional(),
